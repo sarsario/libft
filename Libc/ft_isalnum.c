a60f99df6007b560d84a7d9a72312bb513fdf3c0
osarsari <o.sarsari@gmail.com>
2023-04-07 12:58:33 +0200

    Documented ft_isalnum

--- Libc/ft_isalnum.c
+++ Libc/ft_isalnum.c
@@ -6,11 +6,21 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 13:17:30 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/06 22:59:32 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/07 12:56:47 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+/*
+** Check if the given character is alphanumeric.
+**
+** c: the character to be checked
+**
+** Return:
+** Non-zero value if the character is alphanumeric, 0 otherwise.
+*/
+
 
 int	ft_isalnum(int c)
 {
