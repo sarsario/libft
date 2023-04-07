25c2471aafd3a176165fd55ee5564d59c89e1e14
Oussama Sarsari <osarsari@student.s19.be>
2023-04-07 18:36:08 +0200

    ft_memmove tested and documented

--- Libc/ft_memmove.c
+++ Libc/ft_memmove.c
@@ -6,17 +6,32 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 15:17:46 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/06 23:01:41 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/07 18:33:46 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+/*
+** Copies len bytes from memory area src to memory area dst. The memory areas may
+** overlap: copying takes place as though the bytes in src are first copied into
+** a temporary array that does not overlap src or dst, and the bytes are then
+** copied from the temporary array to dst.
+**
+** dst: A pointer to the destination array where the content is to be copied.
+** src: A pointer to the source of data to be copied.
+** len: The number of bytes to be copied.
+**
+** returns: A pointer to the destination array, dst.
+*/
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char		*dst_char;
 	const char	*src_char;
 
+	if (!dst || !src)
+		return (NULL);
 	dst_char = dst;
 	src_char = src;
 	if (src_char < dst_char)
