a3009f8ba41bfc99b350dd0c2e03501c72c759ef
Oussama Sarsari <osarsari@student.s19.be>
2023-04-07 18:47:06 +0200

    ft_memset tested and documented

--- Libc/ft_memset.c
+++ Libc/ft_memset.c
@@ -6,15 +6,32 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 14:28:53 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/06 23:01:51 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/07 18:46:29 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+/*
+** Sets the first 'len' bytes of the memory area pointed to by 'b' to the value
+** of unsigned char 'c'.
+**
+** b:   A pointer to the memory area to be set.
+** c:   The value to be set. This is passed as an int, but it is converted to an
+**      unsigned char before being written to memory.
+** len: The number of bytes to be set to the value.
+**
+** Returns: A pointer to the memory area 'b'.
+*/
+
 void	*ft_memset(void *b, int c, size_t len)
 {
+	unsigned char *ptr;
+
+	if (!b)
+		return (NULL);
+	ptr = (unsigned char *)b;
 	while (len--)
-		*(unsigned char *)b++ = (unsigned char)c;
+		*ptr++ = (unsigned char)c;
 	return (b);
 }
