5899f225cffc6b51fecf60974f06e7294195d95b
Oussama Sarsari <osarsari@student.s19.be>
2023-04-07 18:57:51 +0200

    ft_strchr tested and documented

--- Libc/ft_strchr.c
+++ Libc/ft_strchr.c
@@ -6,19 +6,30 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 14:46:07 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/06 23:02:03 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/07 18:57:18 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+/*
+** Searches for the first occurrence of the character c (an unsigned char)
+** in the string pointed to by s.
+**
+** s: The string to search in.
+** c: The character to search for.
+**
+** returns: A pointer to the first occurrence of the character c in the
+**          string s, or NULL if the character is not found.
+*/
+
 char	*ft_strchr(const char *s, int c)
 {
 	if (!s)
 		return (NULL);
-	while (*s && c != *s)
+	while (*s && (unsigned char)c != *s)
 		s++;
-	if (c == *s)
+	if ((unsigned char)c == *s)
 		return ((char *)s);
 	return (NULL);
 }
