45c8729c3e51b625e155f11a0d930dbfd3b04933
Oussama Sarsari <osarsari@student.s19.be>
2023-04-07 17:34:52 +0200

    ft_memcmp tested and documented

--- Libc/ft_memcmp.c
+++ Libc/ft_memcmp.c
@@ -6,21 +6,37 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:02:27 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/06 23:01:15 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/07 17:33:29 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+/*
+** Compares the first n bytes of the memory areas s1 and s2.
+**
+** s1: A pointer to the first memory area.
+** s2: A pointer to the second memory area.
+** n: The number of bytes to compare.
+**
+** Returns:
+** An integer less than, equal to, or greater than zero if the first n bytes of s1 are found,
+** respectively, to be less than, to match, or be greater than the first n bytes of s2.
+*/
+
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	uc;
+	const unsigned char	*us1;
+	const unsigned char	*us2;
 
-	while (n--)
+	if (!s1 || !s2 || n == 0)
+		return (0);
+	us1 = (unsigned char *)s1;
+	us2 = (unsigned char *)s2;
+	while (n-- && *us1 == *us2)
 	{
-		uc = *(unsigned char *)s1++ - *(unsigned char *)s2++;
-		if (uc != 0)
-			return ((int)uc);
+		us1++;
+		us2++;
 	}
-	return (0);
+	return (*us1 - *us2);
 }
---
+++ Tests/ft_memcmp.c
@@ -0,0 +1,48 @@
+#include "../libft.h"
+#include <string.h>
+#include <stdio.h>
+
+#define BUF_SIZE 10
+
+void	test_ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	int result_ft = ft_memcmp(s1, s2, n);
+	int result_std = memcmp(s1, s2, n);
+	if (result_ft == result_std)
+		printf("[OK] ft_memcmp: %d, memcmp: %d\n", result_ft, result_std);
+	else
+		printf("[KO] ft_memcmp: %d, memcmp: %d\n", result_ft, result_std);
+}
+
+int main(void)
+{
+    // Test with two identical strings
+    char buf1[BUF_SIZE] = "hello";
+    char buf2[BUF_SIZE] = "hello";
+    int res1 = ft_memcmp(buf1, buf2, strlen(buf1));
+    int res2 = memcmp(buf1, buf2, strlen(buf1));
+    printf("ft_memcmp(\"hello\", \"hello\", %zu) = %d, memcmp(\"hello\", \"hello\", %zu) = %d\n", strlen(buf1), res1, strlen(buf1), res2);
+
+    // Test with two different strings
+    char buf3[BUF_SIZE] = "world";
+    char buf4[BUF_SIZE] = "hello";
+    res1 = ft_memcmp(buf3, buf4, strlen(buf3));
+    res2 = memcmp(buf3, buf4, strlen(buf3));
+    printf("ft_memcmp(\"world\", \"hello\", %zu) = %d, memcmp(\"world\", \"hello\", %zu) = %d\n", strlen(buf3), res1, strlen(buf3), res2);
+
+    // Test with two strings that differ in the last character
+    char buf5[BUF_SIZE] = "hello";
+    char buf6[BUF_SIZE] = "hellp";
+    res1 = ft_memcmp(buf5, buf6, strlen(buf5));
+    res2 = memcmp(buf5, buf6, strlen(buf5));
+    printf("ft_memcmp(\"hello\", \"hellp\", %zu) = %d, memcmp(\"hello\", \"hellp\", %zu) = %d\n", strlen(buf5), res1, strlen(buf5), res2);
+
+    // Test with n = 0
+    char buf7[BUF_SIZE] = "hello";
+    char buf8[BUF_SIZE] = "world";
+    res1 = ft_memcmp(buf7, buf8, 0);
+    res2 = memcmp(buf7, buf8, 0);
+    printf("ft_memcmp(\"hello\", \"world\", 0) = %d, memcmp(\"hello\", \"world\", 0) = %d\n", res1, res2);
+
+    return 0;
+}
