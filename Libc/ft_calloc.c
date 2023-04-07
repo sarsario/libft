bbaa4b1003762d59db55d6acc36d5b763becdd95
osarsari <o.sarsari@gmail.com>
2023-04-07 12:50:00 +0200

    Documented ft_calloc

--- Libc/ft_calloc.c
+++ Libc/ft_calloc.c
@@ -6,17 +6,36 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 08:48:43 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/06 22:59:21 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/07 12:49:30 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 #include <stdlib.h>
 
+/*
+** Allocates memory for an array of count elements of size bytes each and
+** returns a pointer to the allocated memory. The memory is set to zero.
+**
+** @param count The number of elements to allocate memory for.
+** @param size The size of each element to allocate memory for.
+**
+** @return A void pointer to the allocated memory if successful, NULL otherwise.
+*/
+
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	total;
 
+	if (count == 0 || size == 0)
+	{
+		count = 1;
+		size = 1;
+	}
+	total = count * size;
+	if (total / count != size || total / size != count)
+		return (NULL);
 	ptr = (void *)malloc(count * size);
 	if (!ptr)
 		return (NULL);
