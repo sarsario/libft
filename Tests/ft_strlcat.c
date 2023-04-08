14611ef3c160d782456d937023c00b8a10b47436
osarsari <o.sarsari@gmail.com>
2023-04-08 14:39:16 +0200

    ft_strlcat test setup

---
+++ Tests/ft_strlcat.c
@@ -0,0 +1,19 @@
+#include "../libft.h"
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    // 1
+	// When dst and src are both not NULL, and dstsize is larger than the size of dst.
+    // 2
+	// When dst and src are both not NULL, and dstsize is smaller than or equal to the size of dst.
+    // 3
+	// When dst is NULL and src is not NULL, and dstsize is greater than 0.
+    // 4
+	// When dst is not NULL and src is NULL.
+    // 5
+	// When dst is NULL and src is NULL, and dstsize is greater than 0.
+    // 6
+	// When dst is NULL and src is NULL, and dstsize is 0.
+}
