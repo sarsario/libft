5d76121f30540854fe4ed7166acf81e60fa8da87
osarsari <o.sarsari@gmail.com>
2023-04-08 14:39:02 +0200

    ft_strlen protected against null pointer

--- Libc/ft_strlen.c
+++ Libc/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 13:31:43 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/06 23:02:54 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/07 21:53:00 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	len;
 
 	len = 0;
+	if (!s)
+		return (len);
 	while (*s++)
 		len++;
 	return (len);
