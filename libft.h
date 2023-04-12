2d634c7bdbea7a9aa5a996e43ed383b7b038fa43
Oussama Sarsari <osarsari@student.s19.be>
2023-04-12 12:58:06 +0200

    ft_lstdelone added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 12:46:36 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/12 12:45:06 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/12 12:55:33 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,4 +61,5 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 #endif
