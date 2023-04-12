62ce56b8c3a2657eaf612ccb3e6417f070cb2381
Oussama Sarsari <osarsari@student.s19.be>
2023-04-12 12:11:14 +0200

    ft_lstlast implemented and documented

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 12:46:36 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/12 11:58:53 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/12 12:11:00 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,4 +59,5 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 #endif
