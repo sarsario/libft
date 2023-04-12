e4b73cc6bb001e5c5859bbdafc0e8947936eeb91
Oussama Sarsari <osarsari@student.s19.be>
2023-04-12 10:46:56 +0200

    Documented ft_lstnew

--- Bonus/ft_lstnew.c
+++ Bonus/ft_lstnew.c
@@ -6,12 +6,23 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 09:49:40 by osarsari          #+#    #+#             */
-/*   Updated: 2023/04/12 10:21:49 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/04/12 10:45:30 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 #include <stdlib.h>
+
+/*
+** Allocates (with malloc(3)) and returns a new node. The member variable
+** `content` is initialized with the value of the parameter `content`.
+** The variable `next` is initialized to NULL.
+**
+** content:	The content to create the node with.
+**
+** Returns:
+** The new node
+*/
 
 t_list	*ft_lstnew(void *content)
 {
