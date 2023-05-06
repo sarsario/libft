172acfdc161cfc4a60c822fa087058f74851769a
Oussama Sarsari <osarsari@student.s19.be>
2023-05-06 15:28:15 +0200

    Norminette fix

--- Printf/ft_printnbr.c
+++ Printf/ft_printnbr.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 13:55:54 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/06 15:23:32 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/06 15:28:03 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ int	try_putnbr(int n, int *total_printed)
 
 int	try_putnbr_u(unsigned int n, int *total_printed)
 {
-	const char *base = "0123456789";
+	const char	*base = "0123456789";
 
 	return (try_putbase_u(n, base, total_printed));
 }
